0aa1101c9845b43f4dc7641cec2509f88a292671
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-03-03 13:54:11 +0200

    Add files via upload

---
+++ JS/JS3/main3.js
@@ -0,0 +1,93 @@
+// #1
+// // let today = 01032020;
+
+// // if (today<02032020){
+// //    console.log("сьогодны перше число");
+// // }else(today=>02032020);
+// //     console.log(no);
+
+
+// #2
+// let today = 01032020;
+
+// if (today%2==0){
+//    console.log("01032020");
+// }else{
+//     console.log(upppss)};
+
+
+// #3
+// if (5%2==0){
+//     console.log(5%2 + 'грн');
+// }else{
+//     console.log(5%2 + 'євро')}    
+
+
+// #4
+// let maxAge = 55;
+
+// if(maxAge>0 && maxAge<16){
+//   console.log('діти');
+// }else if(maxAge>17 && maxAge<60){
+//     console.log('дорослі');
+// }else if(maxAge>61 && maxAge<100){
+//    console.log('пенсіонери')}     
+
+
+// #5
+// let a,
+//     b,
+//     c;
+// a=Math.floor(Math.random() * (1 - 0 + 1)) + 1;
+// b=Math.floor(Math.random() * (1 - 0 + 1)) + 1;
+// c=Math.floor(Math.random() * (1 - 0 +1 )) + 1;
+// console.log(a);
+// console.log(b);
+// console.log(c)
+
+//     if( a==1 && b==1 && c==1 || a==2 && b==2 && c==2){
+//         console.log('ФАРТ')
+//     }else{
+//         console.log('НЕ ФАРТ')
+//     }        
+
+
+// // #6
+// let name='Dania',
+//     surname='Pereverziev';
+// console.log('Name '+name.length);
+// console.log('Surname '+surname.length);
+// Name=name.length;
+// Surname=surname.length;
+
+// if( Name>4&&Surname>5 ){
+//     console.log(Name + Surname);
+// }   else{
+//        console.log( 'Упс' );
+// }
+
+
+// #7
+// let a;
+
+// a=Math.floor(Math.random()*(1-5+1))+5;
+// console.log(a);
+
+// if(a==1){
+// console.log('1')
+// }else if (a==2){
+//     console.log('2')
+// }else if (a==3){
+//     console.log('3')
+// }else if (a==4){
+//     console.log('4')
+// }else if(a==5){
+//     console.log('5')
+// }   
+
+
+// #8
+// let myAge=11,
+//     VaniaAge=13;
+// prompt(myAge+VaniaAge);
+
