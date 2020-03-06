6b4ad42a181ee4add19674448e388aa1169e0dda
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-03-06 20:51:57 +0200

    Add files via upload

---
+++ JS/JS2/js/python.js
@@ -0,0 +1,55 @@
+// let k1 = "5px",
+//     k2 = "12djd334",
+//     k3 = "12.45asdwe12",
+//     k4 = "qwqweeewq";
+
+//     console.log(Number.parseInt(k1));
+//     console.log(Number.parseInt(k2));
+//     console.log(Number.parseInt(k3));
+//     console.log(Number.parseInt(k4));  
+
+
+// let k1 = "5px",
+//     k2 = "12djd334",
+//     k3 = "12.45asdwe12",
+//     k4 = "qwqweeewq";
+
+//     console.log(Number.parseFloat(k1));
+//     console.log(Number.parseFloat(k2));
+//     console.log(Number.parseFloat(k3));
+//     console.log(Number.parseFloat(k4));  
+
+
+// let random;
+
+//  random = Math.sqrt(Math.floor(Math.random()*(1-10+1))+10,2);
+//  console.log(random);
+
+// let nameA = "My ";
+//     nameB = "name";
+//     nameC = " is";
+    
+// fullName = nameA + nameB + nameC;
+// console.log(fullName + ' Dania');
+
+
+// let result = 5+5+'5';
+
+// console.log(typeof(result));
+
+
+// const num = 20;
+
+// const result = num > 10 && num < 30;
+// console.log(result);
+
+
+// const num = 40
+// const result = num < 10 || num > 30;
+// console.log(result);
+
+
+// console.log(!true);
+// console.log(!false);
+// console.log(!1);
+// console.log(!0);
