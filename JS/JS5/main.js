0f13614961f5617e3d9e5b6be1f2fcc87f86e1df
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-03-15 17:59:58 +0200

    Add files via upload

---
+++ JS/JS5/main.js
@@ -0,0 +1,69 @@
+// #1
+// const Arr1 = [1,5,"4","hello"];
+// const Arr2 = [true,2,{},["a"],222];
+// console.log(Arr1[1]);
+// console.log(Arr2[1]);
+// console.log(Arr1[1] + Arr2[1]);
+
+// #2
+// const Arr1 = [1,5,"4","hello"];
+// Arr1.push(22);
+// console.log(Arr1);
+
+// #3
+// const Arr2 = [true,2,{},["a"],222];
+// for(const Arr3 of Arr2){
+//     console.log(typeof(Arr2));
+// }
+
+// #4
+// const matrix = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ];
+// for(let i=0; i<matrix.length; i++){
+//     console.log(matrix[i]);
+//     for(n=0; n<matrix[i].length; n++){
+//     console.log(matrix[i][n]);
+//     }
+// }
+
+// #5
+// const message = 'Welcome to Ukraine!';
+// console.log(message.split(''));
+// console.log(message.indexOf("l"));
+// console.log(message.split());
+
+// #6
+// let stack=[];
+// stack.unshift(4);
+// stack.unshift(3);
+// stack.unshift(2);
+// stack.push(5);
+// stack.push(6);
+// stack.push(7);
+// stack.push(8);
+// console.log(stack);
+
+
+// #7
+// const stack = [2,3,4,5,6,7,8];
+// const lastNumbers=stack.splice(4,7);
+// console.log(stack);
+// console.log(lastNumbers);
+// console.log(stack);
+
+
+// #8
+// animals=["pig","dog","cat","parrot", "chicken"];
+// const pets=animals.splice(1,3);
+// console.log(animals);
+// console.log(pets)
+
+// #9
+// pets=["dog","cat","parrot"];
+// pets.splice(2);
+// pets.push("shark");
+// pets.push("elephant");
+// console.log(pets);
