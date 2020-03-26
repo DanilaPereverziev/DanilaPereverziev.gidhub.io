3890928155cb9aca5e719ca53ff561efaec44db7
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-03-26 17:15:50 +0200

    Add files via upload

---
+++ JS/JS6/main.js
@@ -0,0 +1,81 @@
+// #1
+// function hello1(){
+//     return console.log("Привіт JavaScript")
+// }
+
+// hello1();
+
+
+// #2
+// let name = "Даня";
+//     function hello2(){
+//          return console.log("Привіт, " + name)
+//     }
+
+// hello2();
+
+
+// #3
+
+// function mul(n, m){
+//     return console.log(n + m, n - m, n * m);
+// }
+// const result = mul(10, 5);
+// console.log(result);
+
+
+// #4
+// function repeat (str, n){
+//     for(let n = 2; n <= 2; n++);{
+//         console.log(str[i]);
+//     }
+// }
+// repeat([1,2,3]2);
+
+
+// #5
+//  function avg() {
+//      let average = 0,
+//          sum = 0,
+//            count = 0;
+//      for (const argument of arguments) {
+//     count++;
+//      }
+// average = sum/count;
+//      console.log(average);
+//  }
+// avg(1,2,3);
+
+// #6
+// function m(a, b) {
+//     mul(a, b);
+//     function mul(a, b) {
+//         return console.log(a + b, a - b, a * b);
+//     }
+// }
+// m(10, 5);
+
+//7
+// function operation (m,n,o){
+//     function o(a, b){
+//         o = a + b;
+//     }
+//     o(1,2);
+//     console.log(m*n*o);
+// }
+// operation(2, 3);
+
+// let a = function(a, b){
+//     return a+b
+// }
+// a(1,2);
+
+
+// #8
+// function addN(n) {
+//     mul(n, 5);
+//     function mul(a, b) {
+//         return console.log(a + b);
+//     }
+// }
+// let result = addN(5);
