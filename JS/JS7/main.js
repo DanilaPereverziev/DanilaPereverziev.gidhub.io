8314a61365901f233ead220c55c413bef22c6088
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-04-02 21:06:17 +0300

    Add files via upload

---
+++ JS/JS7/main.js
@@ -0,0 +1,70 @@
+// #1
+// const dania = {
+//     name: "Даня",
+//     yearsold: 11,
+//     state: "мальчик",
+//     married: False,
+// }
+
+// #2
+// const key = "мова програмування";
+// const dania = {name: "Даня", yearsold: 11, state: "мальчик", married: false,};
+// delete dania.married;
+
+// dania[key] = 'Python';
+// console.log(dania); 
+
+    
+// #3 
+// const fun1 = function fun12(a,b){
+//     return console.log(a + b);
+// }
+// let fun2 = {
+//     num1:2,
+//     num2:3,
+// };
+
+// fun2.fun1 = function fun12(a,b){
+//     return console.log(a + b);
+// }
+// console.log(fun2);
+
+
+// #4
+// const dania = {
+//     name: "Даня",
+//     yearsold: 11,
+//     state: "мальчик",
+//     lauqndvich: "Python"
+//   };
+  
+//   const keys = Object.keys(dania);
+  
+//   for (const key of keys) {
+//     console.log('Value: ', dania[key]);
+//   }
+
+
+// #5
+// const dania={name: "Даня",
+// yearsold: 11,
+// state: "мальчик",
+// lauqndvich: "Python"};
+// console.log(dania);
+// let str="";
+// str="name: Даня, yearsold: 11, state: мальчик, lauqndvich: Python";
+// console.log(str);
+
+
+// #6
+// let cafe= {name: "Republic",
+// width:200,
+// height: 300,
+// personal:5,
+// hr: true, };
+// const c_a_f_e = Object.entries(cafe);
+// console.log(c_a_f_e);
+// function ploshcha(height,width){
+//     return console.log(height * width);
+// }
+// ploshcha(300,200);
