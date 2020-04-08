0a71dd3495c265e921eef7378b1e77ce1bfd22c8
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-04-08 11:59:27 +0300

    Add files via upload

---
+++ JS/JS8/main.js
@@ -0,0 +1,57 @@
+// #1
+// let inputs = document.querySelectorAll(".first-input");
+//     input = document.querySelector("input");
+
+
+// #2
+// let head = document.querySelector(".title-head");
+// head.textContent = 'i love my mum';
+// head.style.color = 'green';
+
+
+// #3
+// let p = document.querySelector(".more-text"),
+//     p2 = document.querySelector(".new-text")
+// p.classList.add("active");
+// p2.classList.remove("new-text");
+
+
+// #4
+// let input = document.querySelector(".first-input"),
+//     p3 = document.querySelector(".more-text"),
+//     sum = 0;
+//     function firstinput(){
+//     sum = input.value*4;
+//     console.log(sum);
+// }
+//     firstinput();
+//     p3.textContent = sum;
+
+
+// #5
+// let input = document.querySelector(".input");
+//    let objGlobal = {
+//         vik: input.value[0],
+//         prizvishe: input.value[1],
+//         imia: input.value[2],
+//         pobatkovi: input.value[3],
+//     };
+//     console.log(objGlobal);
+
+// #6
+// let img = document.querySelector(".img-1");
+// img.src="./img/second.jpg";
+
+// #7
+// let img2 = document.querySelector(".beautiful");
+// img2.src="./img/four.jpg";
+
+// #8
+// let checked = document.querySelectorAll(".checkbox");
+// for(let i = 0; i<checked.length; i++){
+//     if(checked[i].hasAttribute('checked')){
+//         checked[i].removeAttribute('checked');
+//     }else{
+//         checked[i].setAttribute('checked', "");
+//     }
+// }
