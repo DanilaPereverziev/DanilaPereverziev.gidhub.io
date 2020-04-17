85de965340c781eb3ab1e8bfedfe86c2ffb08d1f
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-04-17 19:46:17 +0300

    Add files via upload

---
+++ JS/JS9/jsjsjsjs/main2.js
@@ -0,0 +1,9 @@
+let speed = 10,
+    html = (500,500)
+    x = 50,
+    y = 10,
+    player = document.querySelector(".player");
+    if(keypress === 'Z'){
+        y - speed;
+    }
+    draw.rect(".player", (0,255,255), (x, y))
