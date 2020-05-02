f055c26b1935f63f96282a7785c0a87350aec4b1
DanilaPereverziev <57060439+DanilaPereverziev@users.noreply.github.com>
2020-05-02 15:03:08 +0300

    Add files via upload

---
+++ JS/JS10/main.js
@@ -0,0 +1,59 @@
+// #1
+// let Arr = [1,2,3,4,5,6];
+
+// Arr.forEach(function(item,i){
+//     console.log(Number([i])+1);
+// });
+
+
+
+// #2
+// let inputs = document.querySelectorAll(".foreach"),
+// sbm = document.querySelector(".sbm");
+// sbm.addEventListener("click", function(){
+//         inputs.forEach(function(item,i){
+//             console.log(item.value);
+//     });
+// });
+
+// #3
+// let inputs = document.querySelectorAll(".foreach");
+//     inputs.forEach(function(item,i){
+//         item.addEventListener("click", function(){
+//             console.log(item)
+//         });
+//     });
+
+
+// #4
+// let block = document.querySelectorAll(".block");
+// block.forEach(function(item,i){
+// item.addEventListener("click", function(){
+// if(item.classList.contains("active")){
+//     item.classList.remove("active");
+// }else{
+//     item.classList.add("active");
+// }
+// console.log(i)
+//     });
+// });
+
+
+// #5
+// let block = document.querySelectorAll(".block");
+// block.forEach(function(item,i){
+// item.addEventListener("click", function(){
+// console.log(item);
+//     });
+// });
+
+
+// #6
+// let inputs = document.querySelectorAll("input");
+// inputs.forEach(function(item,i){
+//     if(inputs[i].hasAttribute === false){
+//         inputs[i].setAttribute(checked,"");
+//     }else{
+//         inputs[i].setAttribute("checked","");
+//     }
+// });
